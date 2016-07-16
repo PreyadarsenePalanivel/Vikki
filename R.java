ddae02a58f2ef2041a0b71c5e2b31d6429c8b50b
PreyadarsenePalanivel <darsenep@gmail.com>
2016-07-16 15:18:22 +0530

    Add files via upload

---
+++ R.java
@@ -0,0 +1,15 @@
+    public int reverseNumber(int number){
+         
+        int reverse = 0;
+        while(number != 0){
+            reverse = (reverse*10)+(number%10);
+            number = number/10;
+        }
+        return reverse;
+    }
+     
+    public static void main(String a[]){
+        NumberReverse nr = new NumberReverse();
+        System.out.println("Result: "+nr.reverseNumber(17868));
+    }
+- See more at: http://www.java2novice.com/java-interview-programs/reverse-number/#sthash.dtI4pVEt.dpuf
